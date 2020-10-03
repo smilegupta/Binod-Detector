8de6bba921fa36de0fbf0c6659913c10eea24ea6
Kousik Snai <56123189+kousiksnai99@users.noreply.github.com>
2020-10-03 23:23:03 +0530

    Create binod.py

---
+++ binod.py
@@ -0,0 +1,29 @@
+import os
+
+def isBinod(filename):
+    with open(filename, "r") as f:
+        fileContent = f.read()
+        # Detect all forms of Binod like bInoD
+    if "binod" in fileContent.lower():
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    #Listing the content of this folder 
+    dir_contents = os.listdir()
+
+    nBinod = 0
+    # For each text file, run isBinod for them
+    for item in dir_contents:
+        if item.endswith('txt'):
+            print(f"Detecting Binod in {item}")
+            flag = isBinod(item)
+            if(flag):
+                print(f"Binod found in {item}!!!!!")
+                nBinod +=1
+            else:
+                print(f"Binod not found in {item}")
+    
+    print("*******Binod Detector Summary*******")
+    print(f"{nBinod} files found with Binod hidden into them")
